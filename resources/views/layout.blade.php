73acdbb90a61df4c6e57ac11d94fc86c35901271
Jean-Paul RAKOTOARISON <idweb2mg@gmail.com>
2017-03-22 17:06:41 +0100

    Intégration bootstrap dans layout.blade.php et ajout des dossiers lavarel sous ressources/views

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,8 +6,7 @@ ebm ~ layout.blade.php
 */
 
 ?>
-<!DOCTYPE html>
-  <html lang="{{ config('app.locale') }}">
+ <html lang="{{ config('app.locale') }}">
 
       <head>
 
@@ -27,83 +26,169 @@ ebm ~ layout.blade.php
           <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
-        <meta name="application-name"	content="e-BM" />
-        <meta name="description"		content="Business Model en ligne" />
-        <meta name="keywords" 			content="HTML5 CSS3 JS JQUERY ANGULAR PHP7 MYSQL PDO ORM AJAX MVC POO " />
-        <meta name="author" 			content="Vivien Maillard - Georges-Alexis Kimbidima - JP Rakotoarison"/>
+        <meta name="application-name" content="e-BM" />
+        <meta name="description"    content="Business Model en ligne" />
+        <meta name="keywords"       content="HTML5 CSS3 JS JQUERY ANGULAR PHP7 MYSQL PDO ORM AJAX MVC POO LARAVEL" />
+        <meta name="author"       content="Vivien Maillard - Georges-Alexis Kimbidima - JP Rakotoarison"/>
 
         <!-- Lien css et font police de caracteres specifique -->
-        <link rel="stylesheet" href="css/app.css" />
-        <link rel="stylesheet" href="css/back.css" />
-        <link rel="stylesheet" href="css/front.css" />
-          <!-- Déclaration des balises <meta> et <link> : -->
+        <link rel="stylesheet" href={{ trans('asset/css/app.css')}} />
+        <link rel="stylesheet" href={{ trans('asset/css/back.css')}} />
+        <link rel="stylesheet" href={{ trans('asset/css/front.css')}} />
 
+
+        <!-- == Eventuellement d'autres spacificités à mettre ci-après dans le yield ==   -->
         @yield('head')
-
-    </head>
+  </head>
 
   <body>
+  
+  <header>
+    <div class="container">
 
-    <header>
+      <div class="row">
+        <div class="col-md-7 col-md-offset-1">
+          <h1>
+            <a href="#" title="e-bm">e-BM</a>
+            <br />
+            <small>Modèle économique en ligne</small>
+          </h1>
+        </div> <!-- class="col-md-7 col-md-offset-1"  -->
+
+      </div> <!-- class="row"  -->
+
+            <div class="brand">{{ trans('front/site.title') }}</div>
+            <div class="address-bar">{{ trans('front/site.sub-title') }}</div>
+            <div id="flags" class="text-center"></div>      
+
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+
+                    <nav class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Accueil</span>
+                            <span class="icon-bar">Projet</span>
+                            <span class="icon-bar">Business Model</span>
+                        </button>
+                        <a class="navbar-brand" href="index.html">{{ trans('front/site.title') }}</a>
+                    </nav> <!--  class="navbar-header" -->
 
 
-        <div id="flags" class="text-center"></div>
-        <nav class="navbar navbar-default">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+        </div> <!-- class="col-md-10 col-md-offset-1">  -->
 
-                </div>
-                <div class="collapse navbar-collapse">
 
-                </div>
-            </div>
-        </nav>
+      </div> <!-- class="row"  -->
+
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li {!! classActivePath('/') !!}>
+                        {!! link_to('/', trans('front/site.home')) !!}
+                    </li>
+                    @if(session('statut') == 'visitor' || session('statut') == 'mem')
+                        <li {!! classActivePath('contact/create') !!}>
+                            {!! link_to('contact/create', trans('front/site.contact')) !!}
+                        </li>
+                    @endif
+                    <li {!! classActiveSegment(1, ['articles', 'blog']) !!}>
+                        {!! link_to('articles', trans('front/site.blog')) !!}
+                    </li>
+                    @if(Request::is('auth/register'))
+                        <li class="active">
+                            {!! link_to('auth/register', trans('front/site.register')) !!}
+                        </li>
+                    @elseif(Request::is('password/email'))
+                        <li class="active">
+                            {!! link_to('password/email', trans('front/site.forget-password')) !!}
+                        </li>
+                    @else
+                        @if(session('statut') == 'visitor')
+                            <li {!! classActivePath('login') !!} >
+                                {!! link_to('login', trans('front/site.connection')) !!}
+                            </li>
+                        @else
+                            @if(session('statut') == 'admin')
+                                <li>
+                                    {!! link_to_route('admin', trans('front/site.administration')) !!}
+                                </li>
+                            @elseif(session('statut') == 'mod')
+                                <li>
+                                    {!! link_to('blog', trans('front/site.moderation')) !!}
+                                </li>
+                            @elseif(session('statut') == 'mem')
+                                <li>
+                                    {!! link_to('blog', trans('front/site.projet')) !!}
+                                </li>
+                                @endif
+                                <li>
+                                    {!! link_to('/logout', trans('front/site.logout'), ['id' => "logout"]) !!}
+                                    {!! Form::open(['url' => '/logout', 'id' => 'logout-form']) !!}{!! Form::close() !!}
+                                </li>
+                            @endif
+                    @endif
+                    <li class="dropdown">
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="#"><img width="32" height="32" alt="{{ session('locale') }}"  src="{!! asset('img/' . session('locale') . '-flag.png') !!}" />&nbsp; <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                        @foreach ( config('app.languages') as $user)
+                            @if($user !== config('app.locale'))
+                                <li><a href="{!! url('language') !!}/{{ $user }}"><img width="32" height="32" alt="{{ $user }}" src="{!! asset('img/' . $user . '-flag.png') !!}"></a></li>
+                            @endif
+                        @endforeach
+                        </ul>
+                    </li>
+                </ul>
+
+            </div><!-- class="collapse navbar-collapse"  -->
+
+
+    </div> <!-- class="container"  -->
+
+
+</header>
+    <!-- == Eventuellement d'autres spacificités à mettre ci-après dans le yield ==   -->
         @yield('header')
-    </header>
 
-    <main class="container">
-        @if(session()->has('ok'))
-            @include('partials/error', ['type' => 'success', 'message' => session('ok')])
-        @endif
-        @if(isset($info))
-            @include('partials/error', ['type' => 'info', 'message' => $info])
-        @endif
-        @yield('main')
-    </main>
+  </header>
 
-    <footer>
-          @yield('footer')
-          <p class="text-center"><small>Copyright &copy; e-BM</small></p>
-    </footer>
-    <!-- ========== ici Scripts JS ================  -->
-      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-      <script
-      src="https://code.jquery.com/jquery-3.1.1.js"
-      integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
-      crossorigin="anonymous"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script>
-    /*$.ajaxSetup({
-      headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      }
-    });
-    $(function() {
-      $('#logout').click(function(e) {
-        e.preventDefault();
-        $('#logout-form').submit();
-      })
-    });
-    </script>
 
-    @yield('scripts')
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7 col-md-offset-1">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h2>Accueil</h2>
+              <img src="bootstrap/img/FRMATRICE.jpg" width="" height="" title="Un canvas du BM" alt="Business Model" />
+              
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
+  
+  <footer>
+        <!-- == Eventuellement d'autres spécificités à mettre ci-après dans le yield ==   -->
+        @yield('footer')
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <a href="#" title="alt">Informations</a> - 
+                    <a href="#" title="alt">Mentions légales</a> - 
+                    <a href="#" title="alt">CGU  Conditions Générales d'Utilisation</a> - 
+                    <a href="#" title="alt">Contact</a> 
+                    <br />
+                    <p class="text-center"><small>Copyright &copy; 2017 e-BM. Tous droits réservés</small></p>
+                </div>  
+            </div><!-- class="row"   -->
+         </div> <!-- class="container"   -->
+
+  </footer>
 
   </body>
+
 </html>
