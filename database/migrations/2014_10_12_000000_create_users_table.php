ec026b3d973bb06447942a61b7f029ba2e6a3637
Alexis <newalexisss@gmail.com>
2017-03-22 10:39:42 +0100

    ajout 4 coll ds users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,10 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->integer('ID_ROLES')->unsigned();
             $table->foreign('ID_ROLES')->references('ID_ROLES')->on('ROLES');
+            $table->boolean('seen')->default(false);
+            $table->boolean('valid')->default(false);
+            $table->boolean('confirmed')->default(false);
+            $table->string('confirmation_code')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
